e75a388152cf7e08a62192efd2f750da64d030b9
Brian Lawler <bplawler@gmail.com>
2011-11-28 10:04:46 -0800

    adding some documentation to the skyline project.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-This repository contains all of the code snippets that I am using in my
-focused effort at learning Scala.
+# HelloScala
+
+This project contains a bunch of little projects that I am doing just to learn
+about the Scala programming language
+
+## Boggle
+
+I wrote more about my Boggle code [here](http://bplawler.tumblr.com/post/10569631567/scala-diversion-a-small-project).  This little bit of code is basically
+an overly simplified implementation of the Boggle word game.  I did this just
+to see if I could pick Scala back up after not coding anything in it for a few
+weeks.
+
+## Skyline
+
+The [skyline problem](http://stackoverflow.com/questions/1066234/the-skyline-problem)
+was just a fun little programming problem that I heard about and decided to 
+try out in Scala.  I used this problem as an exercise to learn more about a
+concept called "[folding](http://oldfashionedsoftware.com/2009/07/10/scala-code-review-foldleft-and-foldright/)".  
--- src/main/scala/skyline/skyline.scala
+++ src/main/scala/skyline/skyline.scala
@@ -1,30 +1,69 @@
-class square(p1: Pair[Int, Int], p2: Pair[Int, Int], 
-             p3: Pair[Int, Int], p4: Pair[Int, Int]) {
-  val points = List(p1, p2, p3, p4)
+/**
+ * The square class is used to represent a square that is part of the
+ * skyline to be traced out.  All of the squares are "buildings" along
+ * the ground (y=0) so all we really need to know is where the building
+ * starts (startX), where it ends (endX), and how tall it is (height).
+ */
+class square(val startX: Int, val height: Int, val endX: Int) {
+
+ /**
+  * Method that is called on a square to indicate that it is currently 
+  * "on" and possibly contributing to the skyline.
+  */
   def on(s: skyline) = { s.push(this) }
+
+ /**
+  * Method that is called on a square to indicate that it is currently
+  * "off" and no longer a part of the skyline.
+  */
   def off(s: skyline) = { s.pop(this) }
-  def upperLeftY = { points.foldLeft((0,0))( (r,c)=>{
-    if(c._1 < r._1 && c._2 > r._2) c else r
-  } )._1 }
-  def upperRightY = { points.foldLeft((0,0))( (r,c)=>{
-    if(c._1 > r._1 && c._2 > r._2) c else r
-  } ) }
 }
 
+/**
+ * The skyline class does the work of actually accumulating the points
+ * that make up the skyline.  Buildings are pushed and popped from the
+ * skyline, and each time this happens new corners are added to the 
+ * skyline if the building being added or removed is tall enough.
+ */
 class skyline
 {
   import scala.collection.immutable.ListSet
-  var points = List[Pair[Int, Int]]()
+  var points = List[Int]()
   var currentSquares = ListSet[square]()
-  var currentY = 0
+  def currentY = currentSquares.foldLeft(0)(_ max _.height)
 
   def push(s: square) = {
-    //if [ s.upperLeftY higher than current ] addNewPoints
+    if(s.height > currentY) {
+      points = points :+ s.startX :+ s.height
+    }
     currentSquares = currentSquares + s
   }
 
   def pop(s: square) = { 
     currentSquares = currentSquares - s
-    //if [ s.upperRightY higher than current ] addNewPoints
+    if(s.height > currentY) {
+      points = points :+ s.endX :+ currentY
+    }
   }
+}
+
+class driver {
+  val squares = List(new square(1,11,5), new square(2,6,7), new square(3,13,9), 
+                     new square(12,7,16), new square(14,3,25), new square(19,18,22), 
+                     new square(23,13,29), new square(24,4,28))
+
+  // start with the list of squares...
+  val result = squares.
+    // From this list create a list of the triggers for pushing
+    // and popping buildings from the skyline.  Each element of this is
+    // and Pair containing the X-coordinate of the event and the 
+    // function should be called when we are at this coordinate.
+    foldLeft(List[(Int, skyline => Unit)]())((r,c) => { r :+ (c.startX, c.on _) :+ (c.endX, c.off _) } ).
+    // Sort the list of pairs by the X-coordinate so that we
+    // are able to iterate left to right over the building edges
+    sortWith(_._1 < _._1).
+    // using foldLeft(), send a new skyline instance into the 
+    // ordered list of edges, to turn the buildings on and off
+    // and create the skyline.
+    foldLeft(new skyline())((r,c) => { c._2(r); r } )
 }
